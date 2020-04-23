4dbccec84cfa594f56e5e5fc1d59aa19ff46db63
Petr Chromec <petr.chromec@lmc.eu>
2020-04-23 10:26:00 +0200

    Add serialize to json

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 <!-- There is always Unreleased section on the top. Subsections (Add, Changed, Fix, Removed) should be Add as needed. -->
 ## Unreleased
+- Add functions
+    - `Serialize.toJson`
+    - `Serialize.toJsonPretty`
 
-## 0.1.0 - 
+## 0.1.0 -
 - Initial implementation
--- src/Serializer.fs
+++ src/Serializer.fs
@@ -1,4 +1,27 @@
 namespace Lmc.Serializer
 
+[<RequireQualifiedAccess>]
 module Serialize =
-    let empty = ()
+    module private Json =
+        open Newtonsoft.Json
+        open Newtonsoft.Json.Serialization
+
+        let private options () =
+            JsonSerializerSettings (
+                ContractResolver =
+                    DefaultContractResolver (
+                        NamingStrategy = SnakeCaseNamingStrategy()
+                    )
+            )
+
+        let serialize obj =
+            JsonConvert.SerializeObject (obj, options())
+
+        let serializePretty obj =
+            let options = options()
+            options.Formatting <- Formatting.Indented
+
+            JsonConvert.SerializeObject (obj, options)
+
+    let toJsonPretty: obj -> string = Json.serializePretty
+    let toJson: obj -> string = Json.serialize
